5b2ea54cb91dedee5aab49c883acfdd72a6f9a6c
IazzettaPierluigi <iazzettapierluigi@gmail.com>
2024-02-06 16:17:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <h1>ciao mondo</h1>
+
+    <hr>
+
+    <HelloWorld></HelloWorld>
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <p>hello world</p>
+</template>
+
+<style scoped>
+p {
+    color: red;
+}
+</style>
